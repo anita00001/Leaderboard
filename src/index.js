8b26e5f10eb87249b730fd7aba027bcd2e468149
Anita <sharma.anita00001@gmail.com>
2023-03-14 09:59:33 +0545

    Add a array, add display function and listen click event at submit button

--- src/index.js
+++ src/index.js
@@ -1,13 +1,51 @@
-import _ from 'lodash';
-import './style.css'
+// index.js
+import './style.css';
+import addScore from './modules/add-score.js';
+import { getFromStorage, updateStorage } from './modules/local-storage.js';
 
-function component() {
-  const element = document.createElement('div');
+const submitButton = document.getElementById('submit');
+const table = document.getElementById('scores');
+const yourName = document.getElementById('your-name');
+const yourScore = document.getElementById('your-score');
 
-  // Lodash, currently included via a script, is required for this line to work
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+let leaderBoard = getFromStorage();
 
-  return element;
-}
+leaderBoard = [
+  {
+    name: 'Anita',
+    score: 90,
+  },
+  {
+    name: 'Raj',
+    score: 100,
+  },
+  {
+    name: 'Yog',
+    score: 80,
+  },
+];
 
-document.body.appendChild(component());
+const showScore = () => {
+  updateStorage(leaderBoard);
+  leaderBoard.sort((a, b) => b.score - a.score);
+  table.innerHTML = '';
+  leaderBoard.forEach((item) => {
+    const tr = document.createElement('tr');
+    tr.innerHTML = `
+    <td>${item.name}</td>
+    <td>:</td>
+    <td>${item.score}</td>`;
+    table.appendChild(tr);
+  });
+};
+
+showScore();
+
+submitButton.addEventListener('click', (e) => {
+  e.preventDefault();
+  leaderBoard = addScore(yourName.value, yourScore.value, leaderBoard);
+  updateStorage(leaderBoard);
+  showScore();
+  yourName.value = '';
+  yourScore.value = '';
+});
