3cff8df6554b4edc10dd6b610bcbae8d9c88ca46
Anita <sharma.anita00001@gmail.com>
2023-03-15 15:38:43 +0545

    Modified to fetch data from API

--- src/index.js
+++ src/index.js
@@ -1,51 +1,43 @@
-// index.js
 import './style.css';
 import addScore from './modules/add-score.js';
-import { getFromStorage, updateStorage } from './modules/local-storage.js';
 
+const refresh = document.getElementById('refresh');
 const submitButton = document.getElementById('submit');
 const table = document.getElementById('scores');
 const yourName = document.getElementById('your-name');
 const yourScore = document.getElementById('your-score');
 
-let leaderBoard = getFromStorage();
+const url = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/V4nBNcUDAwYdX0EPGSQ0/scores';
 
-leaderBoard = [
-  {
-    name: 'Anita',
-    score: 90,
-  },
-  {
-    name: 'Raj',
-    score: 100,
-  },
-  {
-    name: 'Yog',
-    score: 80,
-  },
-];
-
-const showScore = () => {
-  updateStorage(leaderBoard);
-  leaderBoard.sort((a, b) => b.score - a.score);
+const showScore = async () => {
   table.innerHTML = '';
-  leaderBoard.forEach((item) => {
-    const tr = document.createElement('tr');
-    tr.innerHTML = `
-    <td>${item.name}</td>
-    <td>:</td>
-    <td>${item.score}</td>`;
-    table.appendChild(tr);
-  });
+
+  await fetch(url)
+    .then((response) => response.json())
+    .then((json) => {
+      const scores = json.result;
+
+      scores.forEach((item) => {
+        const tr = document.createElement('tr');
+        tr.innerHTML = `
+      <td>${item.user}</td>
+      <td>:</td>
+      <td>${item.score}</td>`;
+        table.appendChild(tr);
+      });
+    });
 };
 
-showScore();
+refresh.addEventListener('click', () => {
+  showScore();
+});
 
 submitButton.addEventListener('click', (e) => {
   e.preventDefault();
-  leaderBoard = addScore(yourName.value, yourScore.value, leaderBoard);
-  updateStorage(leaderBoard);
+  addScore(url, yourName.value, yourScore.value);
   showScore();
   yourName.value = '';
   yourScore.value = '';
 });
+
+showScore();
